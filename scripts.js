699ca33038d54d091052d6c7f1a4b7eac5bfb41d
Brian Lau <blau08@gmail.com>
2025-02-09 14:25:46 -0800

    first commit

---
+++ scripts.js
@@ -0,0 +1,6 @@
+// JavaScript functionality for the AHSC Portfolio website
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('AHSC Portfolio website loaded');
+    // Add any additional JavaScript functionality here
+});
